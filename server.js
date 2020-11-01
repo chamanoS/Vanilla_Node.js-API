3ba240e2f7facaf0e0b39e8533d203677092d13d
chamano sidney <chamano.sr@gmail.com>
2020-11-01 11:55:28 +0200

    securing the route

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const http = require('http')
 const products = require('./data/product')
 
 const server = http.createServer((req,res) =>{
-    res.writeHead(200, { 'Content-Type': 'application/json'})
-    res.end(JSON.stringify(products))
+    if(req.url === '/api/products' && req.method === 'GET'){
+        res.writeHead(200, { 'Content-Type': 'application/json'})
+        res.end(JSON.stringify(products))
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json'})
+        res.end(JSON.stringify({message: 'Route Not Found'}))
+    }
+    
 })
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () =>{
