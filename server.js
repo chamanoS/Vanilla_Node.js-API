d6069aee217170158a716061bd2cfa3ffc74298c
chamano sidney <chamano.sr@gmail.com>
2020-11-01 11:48:19 +0200

    deleting node_modules

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http')
+const products = require('./data/product')
 
 const server = http.createServer((req,res) =>{
-
+    res.writeHead(200, { 'Content-Type': 'application/json'})
+    res.end(JSON.stringify(products))
 })
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () =>{
